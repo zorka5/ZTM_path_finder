1d4e3af25dc9d5dc7f7b645f752d548e696f1b2d
Zofia Kubrak <zofiakubrak@gmail.com>
2022-04-05 09:05:48 +0200

    Algorithms modifications

--- ztm_path_finder/Astar.h
+++ ztm_path_finder/Astar.h
@@ -1,28 +1,49 @@
 #pragma once
 #include <memory>
+#include <cstdlib>
 
 #include "Algorithm.h"
 #include "Graph.h"
 
 template <typename T>
-class Dijkstra :
+class Astar :
     public Algorithm
 {
-public:
-    std::string name;
+
+private:
+    double calculate_heuristics(std::shared_ptr<Vertex<T>> current, std::shared_ptr<Vertex<T>> destination);
+public: 
+    std::string name = "A star";
     void find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::shared_ptr<Vertex<T>> end);
 };
 
 template <typename T>
-void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::shared_ptr<Vertex<T>> end) {
-    std::cout << "Dijkstra" << std::endl;
+double Astar<T>::calculate_heuristics(std::shared_ptr<Vertex<T>> current, std::shared_ptr<Vertex<T>> destination) {
+    double h = 1;
+
+    Vertex<T>* curr_ver = current.get();
+    Localization curr_loc = curr_ver->get_loc();
+
+    Vertex<T>* end_ver = destination.get();
+    Localization end_loc = end_ver->get_loc();
+
+    //std::cout << end_loc.x << curr_loc.x << end_loc.y << curr_loc.y;
+    std::cout << "hstart";
+    h = abs(end_loc.x - curr_loc.x) + abs(end_loc.y - curr_loc.y);
+    std::cout << "hend";
+
+    return h;
+}
+
+template <typename T>
+void Astar<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::shared_ptr<Vertex<T>> end) {
+    std::cout << "Astar" << std::endl;
 
     std::vector <std::shared_ptr<Vertex<T>>> vertices = G.get_vertices();
     Edges<T> edges = G.get_edges();
 
     std::shared_ptr<Vertex<T>> c = end;
     Vertex<T>* vy = c.get();
-    //std::cout << vy->get_data() << ", ";
 
     //Set all vertices distances = infinity
     double inf = std::numeric_limits<double>::infinity();
@@ -30,8 +51,11 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
     //map representing previous vertex in the path
     std::map<std::shared_ptr<Vertex<T>>, std::shared_ptr<Vertex<T>>> prev;
 
-    //map representing distances from source vertex
+    //map representing distances from source vertex (cost)
     std::map<std::shared_ptr<Vertex<T>>, double> dist;
+
+    //map representing distance + heuristics
+    std::map<std::shared_ptr<Vertex<T>>, double> priority;
 
     //set do przechowywania posortowanych wierzcho³ków
     typedef std::pair<std::shared_ptr<Vertex<T>>, double> pair_sharedptr_double;
@@ -41,11 +65,12 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
     std::map<std::shared_ptr<Vertex<T>>, bool> visited;
 
     dist[start] = 0;
+    priority[start] = 0;
 
     for (auto& vertex_iterator : vertices) {
         if (vertex_iterator != start) {
             dist[vertex_iterator] = inf;
-            //dist[vertex_iterator] = rand() % 10 + 1;
+            priority[vertex_iterator] = inf;
             prev[vertex_iterator] = nullptr;
             visited[vertex_iterator] = false;
         }
@@ -55,18 +80,17 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
     }
 
     //wypisanie kolejki
-    //std::set<pair_sharedptr_double, myComp> temp = Q;
+    std::multiset<pair_sharedptr_double, myComp> temp = Q;
+
+    std::cout << "Wypisanie kolejki: " << std::endl;
     for (auto& el : Q) {
         Vertex<T>* v = el.first.get();
-        //std::cout << v->get_data() << ": "  << el.second << std::endl;
+        std::cout << v->get_data() << ": "  << el.second << std::endl;
     }
 
     while (!Q.empty()) {
         auto it = Q.begin(); // iterator na 1 element
         std::pair<std::shared_ptr<Vertex<T>>, double> top = *it;
-
-        //Vertex<T>* u = u.get();
-        //std::cout << "Current:" <<  u->get_data()<< std::endl;
 
         std::shared_ptr<Vertex<T>> u = top.first;
 
@@ -84,18 +108,15 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
         if (itr != Q.end()) {
             Q.erase(itr);
             Vertex<T>* vertex = u.get();
-            //std::cout << "usunienie z kolejki vertexa: " << vertex->get_data() << std::endl;
         }
 
         for (auto& v : neighbours) {
             if (visited[v] == true) {
                 Vertex<T>* vd = v.get();
-                //std::cout << "Vertex: " << vd->get_data() << "odwiedzony" << std::endl;;
                 continue;
             }
 
             if (dist[v] > dist[u] + G.get_edge_weight(u, v)) {
-                //std::cout << std::endl<< "mniejsza" << " ";
                 prev[v] = u;
 
                 //Q.decrease_priority(v, alt)
@@ -104,28 +125,23 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
                 if (itr != Q.end()) {
                     Q.erase(itr);
                     dist[v] = dist[u] + G.get_edge_weight(u, v);
-                    pair_sharedptr_double new_set_element(v, dist[v]);
-                    Q.insert(new_set_element);
 
+                    std::cout << "h: " << calculate_heuristics(v, end) << std::endl;
+
+                    priority[v] = dist[v] + calculate_heuristics(v, end);
+                    pair_sharedptr_double new_set_element(v, priority[v]);
+                    Q.insert(new_set_element);
                 }
-                ////std::cout << "Wypisanie kolejki" << std::endl;
-                //for (auto& el : Q) {
-                //    Vertex<T>* v = el.first.get();
-                //    //std::cout << v->get_data() << ": " << el.second << std::endl;
-                //}
+
+                //std::cout << "Wypisanie kolejki" << std::endl;
+                std::cout << std::endl << "Wypisywanie kolejki: " << std::endl;
+                for (auto& el : Q) {
+                    Vertex<T>* v = el.first.get();
+                    std::cout << v->get_data() << ": " << el.second << std::endl;
+                }
             }
-            //std::cout << dist[v] << std::endl;
         }
     }
-
-    for (auto& el : dist) {
-
-        //std::map<std::shared_ptr<Vertex<T>>, double> el;
-        //std::cout << "dist" << std::endl;
-        Vertex<T>* v1 = el.first.get();
-        //std::cout << v1->get_data() << ": " << el.second << std::endl;
-    }
-
 
     //finding a path to end vertex
     std::vector<std::shared_ptr<Vertex<T>>> path;
@@ -147,4 +163,5 @@ void Dijkstra<T>::find_path(Graph<T> G, std::shared_ptr<Vertex<T>> start, std::s
 
     //print cost
     std::cout << std::endl << "Cost:" << dist[end];
+
 }
